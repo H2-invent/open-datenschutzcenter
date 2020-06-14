3e3950a7896245c78e35d8f033211c3190138ede
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-06-14 20:24:28 +0200

    Add logging in cron controller

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\AkademieBuchungen;
 use App\Service\NotificationService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,16 +15,20 @@ class CronController extends AbstractController
     /**
      * @Route("/cron/akademie_update", name="cron_akademie")
      */
-    public function updateCronAkademie(NotificationService $notificationService, Request $request)
+    public function updateCronAkademie(NotificationService $notificationService, Request $request, LoggerInterface $logger)
     {
         $today = new \DateTime();
 
         if ($request->get('token') !== $this->getParameter('cronToken')) {
-            return new JsonResponse(['error' => false, 'hinweis' => 'Token fehlerhaft']);
+            $message = ['error' => false, 'hinweis' => 'Token fehlerhaft', 'token' => $request->get('token'), 'ip' => $request->getClientIp()];
+            $logger->error($message['hinweis'], $message);
+            return new JsonResponse($message);
         }
 
         if ($this->getParameter('cronIPAdress') !== $request->getClientIp()) {
-            return new JsonResponse(['error' => false, 'hinweis' => 'IP Adresse fuer Cron Jobs nicht zugelassen']);
+            $message = ['error' => false, 'hinweis' => 'IP Adresse fuer Cron Jobs nicht zugelassen', 'ip' => $request->getClientIp()];
+            $logger->error($message['hinweis'], $message);
+            return new JsonResponse($message);
         }
 
         $buchungen = $this->getDoctrine()->getRepository(AkademieBuchungen::class)->findOffenByDate($today);
@@ -44,6 +49,9 @@ class CronController extends AbstractController
             $notificationService->sendNotificationAkademie($buchung, $content);
         }
         $em->flush();
-        return new JsonResponse(['error' => false, 'hinweis' => 'Email mit Benachrichtigung verschickt', 'neu' => $countNeu, 'wdh' => $countWdh]);
+        $message = ['error' => false, 'hinweis' => 'Email mit Benachrichtigung zu offenen Kursen in der Akademie verschickt', 'neu' => $countNeu, 'wdh' => $countWdh];
+
+        $logger->info($message['hinweis'], $message);
+        return new JsonResponse($message);
     }
 }
