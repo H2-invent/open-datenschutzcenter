7d0f209e9be187e2952c249be8434f6549b09488
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-06-14 19:59:19 +0200

    Adjust notification email for new acadmie unit

--- src/Controller/CronController.php
+++ src/Controller/CronController.php
@@ -19,27 +19,31 @@ class CronController extends AbstractController
         $today = new \DateTime();
 
         if ($request->get('token') !== $this->getParameter('cronToken')) {
-            return new JsonResponse(['hinweis' => 'Token fehlerhaft']);
+            return new JsonResponse(['error' => false, 'hinweis' => 'Token fehlerhaft']);
         }
 
         if ($this->getParameter('cronIPAdress') !== $request->getClientIp()) {
-            return new JsonResponse(['hinweis' => 'IP Adresse fuer Cron Jobs nicht zugelassen']);
+            return new JsonResponse(['error' => false, 'hinweis' => 'IP Adresse fuer Cron Jobs nicht zugelassen']);
         }
 
         $buchungen = $this->getDoctrine()->getRepository(AkademieBuchungen::class)->findOffenByDate($today);
 
         $em = $this->getDoctrine()->getManager();
+        $countNeu = 0;
+        $countWdh = 0;
         foreach ($buchungen as $buchung) {
             if (!$buchung->getInvitation()) {
                 $content = $this->renderView('email/neuerKurs.html.twig', ['buchung' => $buchung]);
                 $buchung->setInvitation(true);
                 $em->persist($buchung);
+                ++$countNeu;
             } else {
-                $content = $this->renderView('email/errinnerungKurs.html.twig',['buchung'=>$buchung]);
+                $content = $this->renderView('email/errinnerungKurs.html.twig', ['buchung' => $buchung]);
+                ++$countWdh;
             }
             $notificationService->sendNotificationAkademie($buchung, $content);
         }
         $em->flush();
-        return new JsonResponse(['hinweis' => 'Email mit Benachrichtigung verschickt']);
+        return new JsonResponse(['error' => false, 'hinweis' => 'Email mit Benachrichtigung verschickt', 'neu' => $countNeu, 'wdh' => $countWdh]);
     }
 }
