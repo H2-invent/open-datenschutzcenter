65ff8ca4275940efc4c81b54ea35c3b7ce759dad
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-06-19 16:46:13 +0200

    Set Assert in VVT form Specify VVT export

--- src/Entity/VVT.php
+++ src/Entity/VVT.php
@@ -165,12 +165,14 @@ class VVT
     /**
      * @ORM\Column(type="integer")
      * @Assert\NotBlank()
+     * @Assert\NotNull()
      */
     private $beurteilungEintritt;
 
     /**
      * @ORM\Column(type="integer")
      * @Assert\NotBlank()
+     * @Assert\NotNull()
      */
     private $beurteilungSchaden;
 
