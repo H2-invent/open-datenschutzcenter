35e23da073f7e97c73894867a629dcbc90d02c39
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-07-09 10:44:17 +0200

    webpack.config.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,48 @@
 /*
  * Welcome to your app's main JavaScript file!
  *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
  */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+
+
 import $ from 'jquery';
+import 'datatables.net-dt';
 
 global.$ = global.jQuery = $;
-import('bootstrap');
+
+import ('jszip');
 import('datatables.net');
+import ('datatables.net-buttons-dt');
+require('datatables.net-buttons/js/buttons.flash.js')(window, $);
+require('datatables.net-buttons/js/buttons.html5.js')(window, $);
 import('moment');
 import ('vis-network');
 
+
+$(document).ready(function () {
+    setTimeout(function () {
+        var x = document.getElementById("snackbar");
+        x.className = "show";
+        setTimeout(function () {
+            x.className = x.className.replace("show", "");
+        }, 3000);
+    }, 2000);
+
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+
+    $('#data-table').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'csv', 'excel'
+
+        ]
+    });
+});
+
+$(".clickable-row").click(function () {
+    window.location = $(this).data("href");
+});
