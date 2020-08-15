fc33c20c6d813443aa1769f7de7fb16a376cff5a
andreas.holzmann <andreas.holzmann@h2-invent.com>
2020-08-15 13:21:17 +0200

    Add own Frontend Webpack

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,10 +20,9 @@ import('moment');
 
 $(document).ready(function () {
     setTimeout(function () {
-        var x = document.getElementById("snackbar");
-        x.className = "show";
+        $('#snackbar').addClass('show');
         setTimeout(function () {
-            x.className = x.className.replace("show", "");
+            $('#snackbar').removeClass('show');
         }, 3000);
     }, 500);
 
