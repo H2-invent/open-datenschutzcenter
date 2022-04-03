89a2a4baa3a689aad0eca605a276414e0ef0240a
alex <devkral@web.de>
2022-04-03 23:36:25 +0200

    fix dev build

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
+
+cd /var/www/html/
+chown -R docker:docker .
+
 # prod
 
 mkdir -m 777 -p public/build
-npm run build
+su docker -c "npm run build"
 mkdir -m 775 -p public/uploads
 mkdir -m 775 -p public/data
 chmod -R 775 public/build
@@ -13,31 +17,34 @@ chown -R www-data:www-data public/data
 chown -R www-data:www-data var
 
 # dev
-mkdir -p dev_out
-chown -R www-data:www-data dev_out
-chmod -R 775 dev_out
-mkdir -p dev
+mkdir -m 555 -p dev_out
+chown -R docker:docker dev_out
+mkdir -m 775 -p dev
 cd dev
-mkdir -p public
-ln -s ../vendor .
-ln -s ../node_modules .
-for subf in $(ls ../public); do
-    if [ "$subf" != "build" ]; then
-        ln -s "../dev_out/$subf" .
+mkdir -m 755 -p public
+for subf in $(ls -A ../public); do
+    if [ "$subf" != "build" ] && [ "$subf" != "data" ] && [ "$subf" != "uploads" ] && [ "$subf" != "index.php" ]; then
+        ln -s "../../dev_out/public/$subf" ./public
     fi
 done
-for subf in $(ls ../); do
-    if [ "$subf" != "dev" ] && [ "$subf" != "dev_out" ] && [ "$subf" != "public" ] && [ "$subf" != "vendor" ] && [ "$subf" != "node_modules" ] && [ "$subf" != "var" ]; then
-        ln -s "../dev_out/$subf" .
+cp -a "../public/index.php" ./public
+for subf in $(ls -A ../); do
+    if [ "$subf" != "dev" ] && [ "$subf" != "dev_out" ] && [ "$subf" != "public" ]; then
+        if [ -e "../dev_out/$subf" ] && [ "$subf" != "vendor" ] && [ "$subf" != "node_modules" ] && [ "$subf" != "var" ]; then
+            ln -s "../dev_out/$subf" .
+        else
+            ln -s "../$subf" .
+        fi
     fi
 done
 mkdir -m 777 -p public/build
-npm run dev
+su docker -c "npm run dev"
 chmod -R 775 public/build
 mkdir -m 775 -p public/uploads
 mkdir -m 775 -p public/data
 mkdir -p var/cache
 chmod -R 775 var
+chown -R docker:docker .
 chown -R www-data:www-data public/uploads
 chown -R www-data:www-data public/data
 chown -R www-data:www-data var
