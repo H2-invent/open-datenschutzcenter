97bb4378138ac9766870854fd3e6d8d09b418991
Entwicklung <entwicklung@h2-invent.com>
2022-07-11 23:09:42 +0200

    create docker with letsencrypt

--- docker-entrypoint-initdb.d/init-userdb.sql
+++ docker-entrypoint-initdb.d/init-userdb.sql
@@ -1,6 +1,6 @@
-CREATE USER 'jitsiadmin'@'%' IDENTIFIED BY '<open-datenschutzcenter-pw>';
-CREATE DATABASE jitsiadmin;
-GRANT ALL PRIVILEGES ON jitsiadmin.* TO 'jitsiadmin'@'%';
+CREATE USER 'odc'@'%' IDENTIFIED BY '<open-datenschutzcenter-pw>';
+CREATE DATABASE odc;
+GRANT ALL PRIVILEGES ON odc.* TO 'odc'@'%';
 CREATE USER 'keycloak'@'%' IDENTIFIED BY '<keycloak-pw>';
 CREATE DATABASE keycloak;
 GRANT ALL PRIVILEGES ON keycloak.* TO 'keycloak'@'%';
