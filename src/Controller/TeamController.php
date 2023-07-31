5b68b5899d3889aae96d8ba34685ee990fac51c6
kayliz <kaywinnet.liz@gmail.com>
2023-07-31 19:48:23 +0200

    refactor team dropdown

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -342,15 +342,12 @@ class TeamController extends BaseController
             return $this->redirectToRoute('dashboard');
         }
 
-        // only superadmins can edit the team hierarchy, and children of the current node cannot be selected
-        if ($securityService->superAdminCheck($user)) {
-            $availableTeams = $currentTeamService->getAvailableWithoutCurrentAndChildren($user, $team);
-        }
+        $availableTeams = $currentTeamService->getTeamsWithoutCurrentHierarchy($user, $team);
 
         $form = $this->createForm(
             TeamType::class,
             $team,
-            ['teams' => $availableTeams ?? null,]
+            ['teams' => $availableTeams,]
         );
         $form->handleRequest($request);
 
