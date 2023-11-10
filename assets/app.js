0f8126ea524b057e577bd513d0c525816790de8a
Rayvented <reimar.schuh@h2-invent.com>
2023-11-10 17:25:18 +0100

    Install and style DataTables (use it in tasks index)

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,25 @@
 import './styles/tailmater.css';
 import './styles/colors.css';
+import './styles/data_tables.css';
 import './scripts/tailmater.js';
+import DataTable from "datatables.net-dt";
+
+Array.prototype.forEach.call(document.getElementsByClassName('dataTable'), function (element) {
+    new DataTable('#' + element.getAttribute('id'), {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'csv',
+                exportOptions: {
+                    columns: ':not(.hide-in-export)'
+                }
+            },
+            {
+                extend: 'excel',
+                exportOptions: {
+                    columns: ':not(.hide-in-export)'
+                }
+            }
+        ]
+    });
+});
