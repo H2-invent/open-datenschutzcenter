98100a125f6d4dcc2de341a5e9f29c13a7cd6047
Rayvented <reimar.schuh@h2-invent.com>
2023-11-14 13:52:41 +0100

    Organize assets (js, css)

--- assets/app.js
+++ assets/app.js
@@ -1,35 +1,9 @@
 import './styles/tailmater.css';
 import './styles/colors.css';
-import './styles/data_tables.css';
+import './styles/dataTablesTailwind.css';
+import './styles/dataTablesApp.css';
 import './styles/app.css';
 import './scripts/tailmater.js';
-import DataTable from "datatables.net-dt";
-
-Array.prototype.forEach.call(document.getElementsByClassName('dataTable'), function (element) {
-    new DataTable('#' + element.getAttribute('id'), {
-        dom: 'Bfrtip',
-        buttons: [
-            {
-                extend: 'csv',
-                exportOptions: {
-                    columns: ':not(.hide-in-export)'
-                }
-            },
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: ':not(.hide-in-export)'
-                }
-            }
-        ]
-    });
-});
-
-document.querySelectorAll('[data-href]').forEach(function (element) {
-    const link = element.getAttribute('data-href');
-    element.addEventListener('click', function() {
-        window.location.href = link;
-    })
-});
+import './scripts/datatables.js';
 
 document.getElementById('snackbar-trigger')?.click();
---
+++ assets/scripts/datatables.js
@@ -0,0 +1,28 @@
+import DataTable from "datatables.net-dt";
+
+Array.prototype.forEach.call(document.getElementsByClassName('dataTable'), function (element) {
+    new DataTable('#' + element.getAttribute('id'), {
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                extend: 'csv',
+                exportOptions: {
+                    columns: ':not(.hide-in-export)'
+                }
+            },
+            {
+                extend: 'excel',
+                exportOptions: {
+                    columns: ':not(.hide-in-export)'
+                }
+            }
+        ]
+    });
+});
+
+document.querySelectorAll('[data-href]').forEach(function (element) {
+    const link = element.getAttribute('data-href');
+    element.addEventListener('click', function() {
+        window.location.href = link;
+    })
+});
