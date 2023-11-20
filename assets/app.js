7aa3207eae6e4f8cb3d5b494b07b21e09ee26847
Rayvented <reimar.schuh@h2-invent.com>
2023-11-20 18:38:20 +0100

    It's Winter. Replace summernote with trix (applied in task/form)

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,31 @@
 import './bootstrap.js';
+import "trix";
+import './scripts/tailmater.js';
+import './scripts/datatables.js';
+
+import 'trix/dist/trix.css';
 import './styles/materialDesignIcons.css';
 import './styles/tailmater.css';
 import './styles/colors.css';
 import './styles/dataTablesTailwind.css';
 import './styles/dataTablesApp.css';
 import './styles/app.css';
-import './scripts/tailmater.js';
-import './scripts/datatables.js';
 
 import $ from 'jquery';
 global.$ = global.jQuery = $;
 import {initFreeFields} from "./scripts/freeField";
 
 document.getElementById('snackbar-trigger')?.click();
+
+(function() {
+    addEventListener("trix-initialize", function(e) {
+        const file_tools = document.querySelector(".trix-button-group--file-tools");
+        file_tools?.remove();
+    })
+    addEventListener("trix-file-accept", function(e) {
+        e.preventDefault();
+    })
+})();
 
 $(document).ready(function() {
     initFreeFields();
